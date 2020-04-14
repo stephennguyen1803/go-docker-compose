c58f45a399cf964d09f2d107e22c13ea7032e78d
stephennguyen1803 <anhdung.phc@gmail.com>
2020-04-14 13:32:45 +0700

    Add Cide For Demo

--- app.go
+++ app.go
@@ -14,10 +14,75 @@ import (
 	"github.com/callicoder/go-docker-compose/model"
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Order struct {
+	OrderID string
+	Total   float64
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome! Please hit the `/qod` API to get the quote of the day."))
+}
+
+func connectMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://root:admin123@localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client, err
+}
+
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	client, err := connectMongo()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "can not connect Mongo")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	var order Order
+	err = json.NewDecoder(r.Body).Decode(&order)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "can not json decode")
+		return
+	}
+
+	quickstartDatabase := client.Database("MiddleWare")
+	ordersCollection := quickstartDatabase.Collection("Orders")
+
+	_, err = ordersCollection.InsertOne(context.TODO(), bson.D{
+		{Key: "OrderId", Value: order.OrderID},
+		{Key: "Total", Value: order.Total},
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusNotImplemented, err.Error())
+		return
+	}
+
+	var message = "Order: " + order.OrderID + "created success"
+	respondWithJSON(w, http.StatusOK, message)
+	return
 }
 
 func quoteOfTheDayHandler(client *redis.Client) http.HandlerFunc {
@@ -61,6 +126,8 @@ func main() {
 
 	r.HandleFunc("/", indexHandler)
 	r.HandleFunc("/qod", quoteOfTheDayHandler(client))
+
+	r.HandleFunc("/createOrder", createOrder).Methods("POST")
 
 	srv := &http.Server{
 		Handler:      r,
--- go.mod
+++ go.mod
@@ -1,7 +1,11 @@
 module github.com/callicoder/go-docker-compose
 
+go 1.14
+
 require (
 	github.com/go-redis/redis v6.14.2+incompatible
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
+	go.mongodb.org/mongo-driver v1.3.2
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
